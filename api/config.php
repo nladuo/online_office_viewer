17d2b72cf622d797fe679d2547d99ae25b2e88c9
nladuo <kalen25115@gmail.com>
2017-04-06 14:40:55 +0800

    add some comments and add config.php.

---
+++ api/config.php
@@ -0,0 +1,3 @@
+<?php
+
+define("REDIS_BACKEND", '127.0.0.1:6379');
--- api/handle.php
+++ api/handle.php
@@ -4,9 +4,15 @@ date_default_timezone_set('GMT');
 require_once __DIR__."/../vendor/autoload.php";
 require_once "./utils/Uploader.php";
 require_once "./utils/uuid.php";
+require_once "config.php";
 
-
-function response($code, $msg, $data) {
+/**
+ * JSON Response
+ * @param $code
+ * @param $msg
+ * @param $data
+ */
+function json_response($code, $msg, $data) {
     echo json_encode([
         'code' => $code,
         'msg' => $msg,
@@ -15,8 +21,13 @@ function response($code, $msg, $data) {
     exit(0);
 }
 
-function wait_convert($filename) {
-    Resque::setBackend('127.0.0.1:6379');
+/**
+ * Add the ConvertJob to redis and wait worker finishing conversion
+ * @param $filename
+ * @return bool
+ */
+function wait_conversion($filename) {
+    Resque::setBackend(REDIS_BACKEND);
 
     $args = array(
         'filename' => $filename
@@ -36,6 +47,7 @@ function wait_convert($filename) {
     return $code == 4;
 }
 
+// 1. Check the extension of upload file.
 $allowed_exts = ["doc", "docx", "ppt", "pptx", "xls", "xlsx", "odt"];
 
 $upload_dir = '../uploaded_files/';
@@ -43,26 +55,27 @@ $upload_dir = '../uploaded_files/';
 $uploader = new FileUpload('uploadfile');
 
 if (!in_array($uploader->getExtension(), $allowed_exts)) {
-    response(1, "the extension " . $uploader->getExtension() . " is not supported", "");
+    json_response(1, "the extension " . $uploader->getExtension() . " is not supported", "");
 }
 
+// 2. Save the upload file to ../uploaded_files
 $uuid = create_uuid();
 $pdf_name = $uuid . ".pdf";
 $uploader->newFileName = $uuid . "." . $uploader->getExtension();
 
-// Handle the upload
 $result = $uploader->handleUpload($upload_dir);
 
 if (!$result) {
-    response(1, $uploader->getErrorMsg(), "");
+    json_response(1, $uploader->getErrorMsg(), "");
 }
 
-// convert the file
-$is_success = wait_convert($uploader->newFileName);
+// 3. Convert the office file to pdf
+$is_success = wait_conversion($uploader->newFileName);
 
+// 4. Return the result
 if ($is_success) {
     $href = "/uploaded_files/" . $pdf_name;
-    response(0, "success", ["href" => $href]);
+    json_response(0, "success", ["href" => $href]);
 } else {
-    response(1, "convert failed", "");
+    json_response(1, "conversion failed", "");
 }
